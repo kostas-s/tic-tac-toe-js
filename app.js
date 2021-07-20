b4d4f6a1fe73c4add29301d24af4ffd0c454c9d2
kostas-s <65230255+kostas-s@users.noreply.github.com>
2021-07-21 01:45:03 +0300

    gameboard and gamecontroller early implementation

--- app.js
+++ app.js
@@ -1,3 +1,78 @@
-const GameBoard = () => {
+const GameBoard = (() => {
 
-}
+    const gameboardArray = ["", "", "", "", "", "", "", "", ""];
+    const gameboardSection = document.querySelector(".gameboard")
+
+    const render = () => {
+        // Refresh gameboard display
+
+        gameboardSection.innerHTML = "";
+        for (let i = 0; i < gameboardArray.length; i++) {
+            const newBlock = document.createElement("div");
+            newBlock.classList.add("block");
+            newBlock.dataset.value = i;
+            newBlock.innerText = gameboardArray[i];
+            newBlock.addEventListener("click", (evt) => { clickedBlock(evt.target) });
+            gameboardSection.appendChild(newBlock);
+        }
+    }
+
+
+    const clickedBlock = (target) => {
+        // Check if block occupied call to advance turn accordingly
+
+        if (target.innerText === "") {
+            target.innerText = GameController.currentTurnSymbol();
+            GameController.newTurn();
+        }
+    }
+
+
+    const clearArray = () => {
+        // Clears array WITHOUT refreshing. Don't forget to refresh calling render
+
+        for (let i = 0; i < gameboardArray.length; i++) {
+            gameboardArray[i] = "";
+        }
+    }
+
+    return { render, clearArray };
+})();
+
+
+const GameController = (() => {
+
+    const nextTurnSymbolDisplay = document.querySelector(".next-turn-symbol");
+    let nextTurnSymbol = "X";
+
+    const startGame = () => {
+        // New game, clears board and starts next turn
+
+        GameBoard.clearArray();
+        GameBoard.render();
+        nextTurnSymbol = "X";
+    };
+
+    const newTurn = () => {
+        // Advances turn and calls to evaluate game
+
+        (nextTurnSymbol === "X") ? nextTurnSymbol = "O" : nextTurnSymbol = "X"
+        nextTurnSymbolDisplay.innerText = nextTurnSymbol;
+        evaluateGame();
+    };
+
+    const evaluateGame = () => {
+        // Checks for win / draw conditions 
+
+        console.log("Check for game over...");
+    };
+
+    // Just used to communicate current turn to GameBoard
+    const currentTurnSymbol = () => {
+        return nextTurnSymbol;
+    };
+
+    return { startGame, newTurn, currentTurnSymbol };
+})();
+
+GameController.startGame();
